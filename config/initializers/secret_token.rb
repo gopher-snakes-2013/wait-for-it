631294210b7d16a350358a973605462663dfe722
natatat <natuduwela@gmail.com>
2013-11-07 11:38:07 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WaitForIt::Application.config.secret_token = 'c98a898c517b7ae257b884747879a6d5523ae0793953c737800a1c5b3895679a47f16561e50f7f5ddf3d8df9ad6f062c7813927be289b80be6c9494064f78304'
