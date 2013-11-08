bf1eb2e7eb58e441111c6aeb649943a1d19681fe
natatat <natuduwela@gmail.com>
2013-11-07 21:05:39 -0800

    update works, without event delegation though

---
+++ app/assets/javascripts/update.js
@@ -0,0 +1,11 @@
+var update = function() {
+  var id = $(this).data("id");
+  var text = $(this).text();
+  var formTemplate = '<form action="/reservations/'+id+'" class="update" method="post"><input name="_method" type="hidden" value="put"><input name="reservation[party_size]" value="'+text+'"></form>';
+  $(this).html(formTemplate);
+  $(".party-size").unbind();
+}
+
+$(document).ready(function(){
+  $(".party-size").on("click", update);
+})
