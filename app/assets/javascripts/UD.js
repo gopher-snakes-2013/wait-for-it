1e6736e63a2e5ccbc1647f4361da87e8434c0c9e
natatat <natuduwela@gmail.com>
2013-11-08 14:41:20 -0800

    ajax and js for updating the wait time on screen, need to event delegate new events thru ajax

--- app/assets/javascripts/UD.js
+++ app/assets/javascripts/UD.js
@@ -1,8 +1,25 @@
-var ReservationActions = {
-  delegations: function() {
+var reservationActions = {
+  init: function() {
+    $("#new_reservation").on("ajax:success", this.addReservation);
+    $("#new_reservation").on("ajax:error", this.errorMessage);
+
     $(".party-size").on("click", this.update);
     $(".rezzy").on("mouseenter", this.showDelete);
     $(".rezzy").on("mouseleave", this.hideDelete);
+  },
+
+  addReservation: function(e, reservationPartial) {
+    $(".reservation-list").append(reservationPartial);
+
+    $("#reservation_name").val("");
+    $("#reservation_party_size").val("");
+    $("#reservation_phone_number").val("");
+    $("#reservation_wait_time").val("");
+  },
+
+  errorMessage: function(e, xhr) {
+    var error = xhr.responseJSON.error_message;
+    $(".error-message").html(error);
   },
 
   update: function() {
@@ -20,8 +37,41 @@ var ReservationActions = {
   hideDelete: function() {
     $(this).find(".delete").addClass("hidden");
   }
-}
+};
+
+var timeToWait = {
+
+  init: function() {
+    $(".submit_button").on("click", this.startInterval);
+  },
+
+  countdown: function() {
+    if (waitTime > 1) {
+      waitTime = waitTime -1;
+      $(".rezzy").last().find(".wait-time").html(waitTime);
+    } else {
+      clearInterval(interval);
+      $(".rezzy").last().find(".times-up").html("done");
+    }
+  },
+
+  startInterval: function() {
+    waitTime = $("#reservation_wait_time").val();
+    $(".wait-time").html(waitTime);
+
+    interval = setInterval(function(){ timeToWait.countdown() }, 60000);
+  }
+
+    // want to get the wait time... $("#reservation_wait_time").val()
+    // initialize countdown and have it update the wait time on the screen
+
+    // how to tie into the database...
+    // get a json object from ajax that poops the reservation on the screen without reload,
+    // tie in the javascript function to the wait_time held in the database?
+    // have it update on the screen?
+};
 
 $(document).ready(function(){
-  ReservationActions.delegations();
-})
+  reservationActions.init();
+  timeToWait.init();
+});
