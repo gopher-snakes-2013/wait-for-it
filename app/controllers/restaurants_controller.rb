1fd09d0d2140269f0fee26625fe378d44b778002
Apprentice <apprentice@dbc-02.local>
2013-11-09 01:03:43 -0800

    nested routes?:

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,21 +1,22 @@
 class RestaurantsController < ApplicationController
 
-	def new
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
     @restaurant = Restaurant.new
   end
 
   def create
     @restaurant = Restaurant.new(params[:restaurant])
     if @restaurant.save
-      redirect_to root_path
+      session[:restaurant_id] = @restaurant.id
+      redirect_to restaurant_reservations_path(session[:restaurant_id])
     else
       flash[:error] = "Try Again!"
       redirect_to new_restaurant_path
     end
-	end
-
-  def index
-    @restaurants = Restaurant.all
   end
 
 end
