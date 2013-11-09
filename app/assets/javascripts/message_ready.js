f79f2829d1ec8d32df7d1c5e0217ae7452307e82
Jeff Edwards <js_edwards@yahoo.com>
2013-11-08 17:16:38 -0800

    deleting commented out code

--- app/assets/javascripts/message_ready.js
+++ app/assets/javascripts/message_ready.js
@@ -1,9 +1,7 @@
 $(document).ready(function(){
 
   $('.rezzy').on("click",".message-ready", function(){
-    // event.preventDefault();
     var guest_id = $(this).closest('tr').data("id")
-    console.log("event listener for message ready called", $(this).closest('tr').data("id"))
     var bound_message_guest_ready = message_guest_ready.bind(this)
     bound_message_guest_ready(guest_id);
   })
