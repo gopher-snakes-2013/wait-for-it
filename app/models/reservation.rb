3674000188a795872df60373e36692844a354b7e
nat <natuduwela@gmail.com>
2013-11-09 09:54:04 -0800

    update all wait times method added, and tests

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,16 +1,17 @@
 class Reservation < ActiveRecord::Base
   attr_accessible :name, :party_size, :phone_number, :wait_time, :estimated_seat_time
+  belongs_to :restaurant
 
-	validates_presence_of :name, :party_size
-	validates_numericality_of :party_size, :wait_time
+  validates_presence_of :name, :party_size
+  validates_numericality_of :party_size, :wait_time
 
   phony_normalize :phone_number, :default_country_code => 'US'
-	validates_plausible_phone :phone_number, :presence => true
-	validates_plausible_phone :phone_number, :country_code => '1'
+  validates_plausible_phone :phone_number, :presence => true
+  validates_plausible_phone :phone_number, :country_code => '1'
 
   before_save :add_plus_phone_number
+  before_update :update_all_wait_times
   after_create :send_text_upon_new_reservation
-  belongs_to :restaurant
 
   def add_plus_phone_number
     self.phone_number = "+" + self.phone_number
@@ -20,4 +21,14 @@ class Reservation < ActiveRecord::Base
     TwilioHelper.send_on_waitlist(self.phone_number,
       "Hi #{self.name}, you've been added to the waitlist. Your wait is approximately #{self.wait_time} minutes.")
   end
+
+  def update_all_wait_times
+    updated_wait = self.wait_time
+    Reservation.all.each do |reservation|
+      if reservation.id > self.id
+        reservation.update_attribute(wait_time: reservation.wait_time + updated_wait)
+      end
+    end
+  end
+
 end
