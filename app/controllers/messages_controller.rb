58437496882c96fb3387bc6585341c5e1206466a
Jeff Edwards <js_edwards@yahoo.com>
2013-11-08 17:09:35 -0800

    notified_table_ready updated to true when text message sent

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,8 @@ class MessagesController < ApplicationController
 
   def create
     reservation = Reservation.find(params[:guest_id])
+    reservation.notified_table_ready = true
+    reservation.save
     TwilioHelper.table_ready(reservation.phone_number)
     render json: {reservation: reservation}.to_json
   end
--- spec/models/reservation_spec.rb
+++ spec/models/reservation_spec.rb
@@ -8,8 +8,8 @@ describe Reservation do
   it { should validate_numericality_of :party_size }
   it { should validate_numericality_of :wait_time }
 
-  it 'should default notified_table_ready to false' do 
-    let(:reservation) {Restaurant.create(name:"Jeff", phone_number:"14154154000",wait_time:40)}
-    reservation.notified_table_ready.should == false
+  it 'should default notified_table_ready to nil' do 
+    @reservation = Reservation.create(name:"Jeff", phone_number:"14154154000",wait_time:40)
+    @reservation.notified_table_ready.should be nil
   end
 end
