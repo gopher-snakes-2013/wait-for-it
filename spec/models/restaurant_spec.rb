296cffd55834e355d8a07283330587a4829f01ff
natatat <natuduwela@gmail.com>
2013-11-10 15:04:38 -0800

    restaurant model - added validation for presence of name

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 describe Restaurant do
 	it { should have_many(:reservations) }
-	it { should validate_presence_of :email }
+  it { should validate_presence_of :email }
+	it { should validate_presence_of :name }
 	it { should validate_presence_of :password }
 	it { should validate_uniqueness_of :email }
   it { should have_secure_password }
