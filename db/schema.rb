69b4bed6a8a041a8f174b6f2ef7b6591c2090b2d
Paul Chong <paulchong.nz@gmail.com>
2013-11-10 17:58:37 -0800

    creating tests for updated root_path view

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131110014614) do
+ActiveRecord::Schema.define(:version => 20131110031629) do
 
   create_table "reservations", :force => true do |t|
     t.string   "name"
