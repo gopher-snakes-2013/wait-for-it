907ad3960dc566d31f548a4e61f1e591193c14f3
natatat <natuduwela@gmail.com>
2013-11-11 22:15:07 -0800

    cleaning things up

--- app/assets/javascripts/reservations/reservations_index.js
+++ app/assets/javascripts/reservations/reservations_index.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+  $("#main-nav").on("click", ".restaurant-name", showNav)
   $(".submit_button").prop("value", "+")
 })
+
+function showNav(e){
+  e.preventDefault()
+  if ($(".subnav").hasClass("hidden")) {
+    $(".subnav").slideDown("slow")
+    $(".subnav").removeClass("hidden")
+  } else {
+    $(".subnav").slideUp("slow")
+    $(".subnav").addClass("hidden")
+  }
+
+}
