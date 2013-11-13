28b546dc27c2475667c55e3957f807e0ab2bffa0
natatat <natuduwela@gmail.com>
2013-11-12 19:13:00 -0800

    model methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -86,4 +86,57 @@ class Reservation < ActiveRecord::Base
   def update_restaurant_wait_time
     self.restaurant.update_max_wait_time
   end
+
+  def time_range_display_start
+    minutes = self.estimated_seat_time.localtime.strftime("%M")
+    hour = self.estimated_seat_time.localtime.strftime("%l")
+    time = round_up(hour, minutes)
+    start_mins = time[:minutes]
+    start_hour = time[:hour]
+    start_time = start_hour + ":" + start_mins
+  end
+
+  def time_range_display_end
+    minutes = self.estimated_seat_time.localtime.strftime("%M").to_i + 10
+    hour = self.estimated_seat_time.localtime.strftime("%l")
+    am_pm = self.estimated_seat_time.localtime.strftime("%P")
+    time = round_up(hour, minutes.to_s)
+    end_mins = time[:minutes]
+    end_hour = time[:hour]
+    end_time = end_hour + ":" + end_mins + am_pm
+  end
+
+  def round_up(hour, mins)
+    rounded_minutes = round_minutes(mins.to_i)
+    hour = hour.to_i
+    if rounded_minutes == 60
+      rounded_minutes = 0
+    elsif rounded_minutes > 60
+      rounded_minutes = 5
+      if hour < 12
+        hour += 1
+      else
+        hour = 1
+      end
+    end
+
+    { hour: hour.to_s, minutes: format(rounded_minutes) }
+  end
+
+  def round_minutes(minutes)
+    if minutes % 5 == 0
+      minutes
+    else
+      minutes + (5 - minutes % 5)
+    end
+  end
+
+  def format(minutes)
+    if minutes < 10
+      "0" + minutes.to_s
+    else
+      minutes.to_s
+    end
+  end
+
 end
