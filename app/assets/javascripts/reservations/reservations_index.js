a2f138ce11b716974fe7f4bb84cf3c2e3d2fdf4a
Laura Mead <lauraemead7@gmail.com>
2013-11-13 10:10:43 -0800

    not done with the JS for the archive button

--- app/assets/javascripts/reservations/reservations_index.js
+++ app/assets/javascripts/reservations/reservations_index.js
@@ -1,15 +1,21 @@
 $(document).ready(function(){
-  $("#main-nav").mouseenter(showNav).mouseleave(hideNav);
-  $(".submit_button").prop("value", "+")
+	$("#main-nav").mouseenter(showNav).mouseleave(hideNav);
+	$(".submit_button").prop("value", "+")
+	$(".archive").click(archive); 
+
+	function archive(e) {
+		$(".archive-button").append("Archived!");
+		$(".archive").remove()
+	}
+
+	function showNav(e){
+		e.preventDefault()
+		$(".subnav").slideDown("slow")
+		$(".subnav").removeClass("hidden")
+	}
+
+	function hideNav(e){
+		$(".subnav").slideUp("slow")
+		$(".subnav").addClass("hidden")
+	}
 })
-// you're polluting the global namespace. namespace it using object literals/modules, etc.
-function showNav(e){
-  e.preventDefault()
-    $(".subnav").slideDown("slow")
-    $(".subnav").removeClass("hidden")
-}
-// you're polluting the global namespace. namespace it using object literals/modules, etc.
-function hideNav(e){
-    $(".subnav").slideUp("slow")
-    $(".subnav").addClass("hidden")
-}
