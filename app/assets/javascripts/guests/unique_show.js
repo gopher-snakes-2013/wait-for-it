2c97de1ef7fdbfee60d5cadaa0378be059cc4567
natatat <natuduwela@gmail.com>
2013-11-13 12:00:51 -0800

    object literals in unique show js

--- app/assets/javascripts/guests/unique_show.js
+++ app/assets/javascripts/guests/unique_show.js
@@ -1,32 +1,34 @@
 $(document).ready(function(){
-  $("#guest-content").on("click", "div.cancel-button", confirm)
-  $("#guest-content").on("click", "div.confirm-button", statusCancelled)
+  $("#guest-content").on("click", "div.cancel-button", Status.confirm)
+  $("#guest-content").on("click", "div.confirm-button", Status.cancel)
 })
 
-function statusCancelled() {
-  var restaurant_id = $("#guest-content").data("restaurant-id")
-  var id = $("#guest-content").data("id")
-  $.ajax({
-    url: "/restaurants/"+restaurant_id+"/reservations/"+id,
-    type: "put",
-    dataType: "json",
-    data: { restaurant_id: restaurant_id, id: id, reservation: { status: "Cancelled" } }
-  }).done(function(){
-    $(".confirmation-message").removeClass("hidden")
-    $("div.confirm-button").addClass("hidden")
-    $("span.ha").html(" don't")
-  }).fail(function(){
-    $button = $("div.confirm-button")
-    $button.removeClass("confirm-button").addClass("cancel-button")
-    $button.find("span").html("cancel")
-    $(".confirmation-message").removeClass("hidden")
-    $(".confirmation-message").html("Please try again.")
-  })
-}
+var Status = {
+  cancel: function() {
+    var restaurant_id = $("#guest-content").data("restaurant-id")
+    var id = $("#guest-content").data("id")
+    $.ajax({
+      url: "/restaurants/"+restaurant_id+"/reservations/"+id,
+      type: "put",
+      dataType: "json",
+      data: { restaurant_id: restaurant_id, id: id, reservation: { status: "Cancelled" } }
+    }).done(function(){
+      $(".confirmation-message").removeClass("hidden")
+      $("div.confirm-button").addClass("hidden")
+      $("span.ha").html(" don't")
+    }).fail(function(){
+      $button = $("div.confirm-button")
+      $button.removeClass("confirm-button").addClass("cancel-button")
+      $button.find("span").html("cancel")
+      $(".confirmation-message").removeClass("hidden")
+      $(".confirmation-message").html("Please try again.")
+    })
+  }
 
-function confirm() {
-  var $cancelButton = $(this)
-  $cancelButton.removeClass("cancel-button")
-  $cancelButton.addClass("confirm-button")
-  $cancelButton.find("span").html("confirm")
+  confirm: function() {
+    var $cancelButton = $(this)
+    $cancelButton.removeClass("cancel-button")
+    $cancelButton.addClass("confirm-button")
+    $cancelButton.find("span").html("confirm")
+  }
 }
