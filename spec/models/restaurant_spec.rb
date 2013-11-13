0d04adb52ef3ece998db0cff996608fc615eb991
natatat <natuduwela@gmail.com>
2013-11-12 20:01:31 -0800

    testing restaurant model. covered.

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -10,4 +10,35 @@ describe Restaurant do
 
   it { should_not allow_value("blah").for(:email) }
   it { should allow_value("blah@email.com").for(:email) }
+
+  before(:each) do
+    @restaurant = Restaurant.create(name: "Sourdough Kitchen", email: "sour@kitchen.com", password: "password", password_confirmation: "password")
+    @reservation = @restaurant.reservations.new
+    @reservation.name = "Jeff"
+    @reservation.party_size = 1
+    @reservation.phone_number = "555-555-5555"
+    @reservation.wait_time = 20
+    @reservation.save
+
+    @reservation_2 = @restaurant.reservations.new
+    @reservation_2.name = "Cindy"
+    @reservation_2.party_size = 2
+    @reservation_2.phone_number = "555-555-5555"
+    @reservation_2.wait_time = 10
+    @reservation_2.save
+  end
+
+  describe "#max_wait_time" do
+    it "should return the max wait time for the restaurant" do
+      expect(@restaurant.max_wait_time).to eq(20)
+    end
+  end
+
+  describe "#current_reservations" do
+    it "should return an array of current reservations sorted by seat time" do
+      expect(@restaurant.current_reservations).to eq([@reservation_2, @reservation])
+    end
+  end
+
 end
+
