d17bd68a28a34ddd6273e07e7eb6be55ffa2d76b
Jeff Edwards <js_edwards@yahoo.com>
2013-11-14 13:59:57 -0800

    renamed Message object and methods to send text when table ready

--- app/assets/javascripts/reservations/message_ready.js
+++ app/assets/javascripts/reservations/message_ready.js
@@ -1,29 +1,31 @@
+var Message = {
+  sendTextTableReady: function(reservationId){
+    var that = this
+    $.ajax({
+      url: "/messages",
+      type: "post",
+      data: {id: reservationId},
+      data_type: "json"
+    }).done(function(){
+      $(that).removeClass("message-ready")
+      $(that).html("sent")
+      $(that).addClass("ready-message-sent")
+      $("error-message").empty()
+    }).error(function(){
+      $("error-message").text("SMS failed :(")
+    })
+  },
+  SMSButtonEvent: function(){
+    var self = this;
+    $(".table-body").on("click", "div.message-ready", function(){
+      var reservationId = $(this).closest(".reservation").data("id")
+      var boundSendTextTableReady = self.sendTextTableReady.bind(this)
+      boundSendTextTableReady(reservationId)
+    })
+  }
+}
+
 $(document).ready(function(){
-
-  $(".table-body").on("click", "div.message-ready", function(){
-    var reservationId = $(this).closest(".reservation").data("id")
-    var boundMessageGuestReady = message.GuestReady.bind(this)
-    boundMessageGuestReady(reservationId)
-  })
-
+  Message.SMSButtonEvent();
 })
 
-var message = {
-GuestReady: function(reservationId){
-  var that = this
-  $.ajax({
-    url: "/messages",
-    type: "post",
-    data: {id: reservationId},
-    data_type: "json"
-
-  }).done(function(){
-    $(that).removeClass("message-ready")
-    $(that).html("sent")
-    $(that).addClass("ready-message-sent")
-    $("error-message").empty()
-  }).error(function(){
-    $("error-message").text("SMS failed :(")
-  })
-}
-}
