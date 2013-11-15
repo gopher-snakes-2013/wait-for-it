eb62db2eedc1630d50e5f75c500dca6222959f5f
Jeff Edwards <js_edwards@yahoo.com>
2013-11-15 08:57:09 -0800

    update restaurant name in twilio helper

--- app/helpers/twilio_helper.rb
+++ app/helpers/twilio_helper.rb
@@ -8,7 +8,7 @@ module TwilioHelper
           from: "+14159926163")
   end
 
-  def self.table_ready(phone_number, retaurant_name)
+  def self.table_ready(phone_number, restaurant_name)
        @client = Twilio::REST::Client.new ENV['ACCOUNT_SID'], ENV['AUTH_TOKEN']
         @message = @client.account.messages.create(
           body: "Your table at #{restaurant_name} is now ready!",
